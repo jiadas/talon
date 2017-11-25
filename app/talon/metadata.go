8156d7fa1fd114436f33a37898caea012998c945
jia <jiada8866@126.com>
2017-11-25 23:39:04 +0800

    improve metadata

--- app/talon/metadata.go
+++ app/talon/metadata.go
@@ -4,15 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"path"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type meta struct {
 	Shards []struct {
 		ID     int    `json:"id"`
 		Cursor string `json:"cursor"`
-	}
+	} `json:"shards"`
 }
 
 func readOrEmpty(fn string) ([]byte, error) {
@@ -25,9 +28,23 @@ func readOrEmpty(fn string) ([]byte, error) {
 	return data, nil
 }
 
+func writeSyncFile(fn string, data []byte) error {
+	f, err := os.OpenFile(fn, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(data)
+	if err == nil {
+		err = f.Sync()
+	}
+	f.Close()
+	return err
+}
+
 func (t *Talon) LoadMetadata() error {
-	fn := path.Join(t.dataPath, "talon.dat")
-	data, err := readOrEmpty(fn)
+	fileName := path.Join(t.getOpts().DataPath, "talon.dat")
+	data, err := readOrEmpty(fileName)
 	if err != nil {
 		return err
 	}
@@ -38,11 +55,46 @@ func (t *Talon) LoadMetadata() error {
 	var m meta
 	err = json.Unmarshal(data, &m)
 	if err != nil {
-		return fmt.Errorf("failed to parse metadata in %s - %s", fn, err)
+		return fmt.Errorf("failed to parse metadata in %s - %s", fileName, err)
 	}
 
 	for _, s := range m.Shards {
 		t.cursorMap[s.ID] = s.Cursor
 	}
+	return nil
+}
+
+// todo 每隔一段时间就PersistMetadata，因为程序突然挂了不会执行写文件，被supervisor重新拉起就还是加载的很早之前的talon.dat
+
+func (t *Talon) PersistMetadata() error {
+	fileName := path.Join(t.getOpts().DataPath, "talon.dat")
+
+	log.WithField("dataPaht", fileName).Info("persisting cursor metadata")
+
+	js := make(map[string]interface{})
+	shards := []interface{}{}
+	for id, cursor := range t.cursorMap {
+		shardData := make(map[string]interface{})
+		shardData["id"] = id
+		shardData["cursor"] = cursor
+		shards = append(shards, shardData)
+	}
+	js["shards"] = shards
+
+	data, err := json.Marshal(&js)
+	if err != nil {
+		return err
+	}
+
+	tmpFileName := fmt.Sprintf("%s.%d.tmp", fileName, rand.Int())
+	err = writeSyncFile(tmpFileName, data)
+	if err != nil {
+		return err
+	}
+	err = os.Rename(tmpFileName, fileName)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
